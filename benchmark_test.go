ed7f5650edcf13d2deaef6255fe76e3ebaab1bcb
xeoncross <david@xeoncross.com>
2019-05-25 23:34:43 -0500

    Added net/smtp real-usage benchmark

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,10 +1,24 @@
 package smtpd
 
 import (
+	"io"
+	"io/ioutil"
 	"net"
+	"net/smtp"
 	"net/textproto"
 	"testing"
 )
+
+func PickRandomPort() (port string, err error) {
+	var listener net.Listener
+	listener, err = net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return
+	}
+	port = listener.Addr().String()
+	err = listener.Close()
+	return
+}
 
 // Benchmark the mail handling without the network stack introducing latency.
 func BenchmarkRawProcessingSequence(b *testing.B) {
@@ -45,5 +59,52 @@ func BenchmarkRawProcessingSequence(b *testing.B) {
 		sendRecv(client, "DATA", 354)
 		sendRecv(client, mimeHeaders+"Test message.\r\n.", 250)
 		sendRecv(client, "QUIT", 221)
+	}
+}
+
+func BenchmarkNetSMTP(b *testing.B) {
+
+	addr, err := PickRandomPort()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	var bytesReceived int
+	server := &Server{
+		Addr:    addr,
+		MaxSize: 100000,
+		Handler: func(bytesRead int, remoteAddr net.Addr, from string, to []string, header textproto.MIMEHeader, body io.Reader) (err error) {
+			_, err = io.Copy(ioutil.Discard, body)
+			return
+		},
+		HandlerSuccess: func(bytesRead int, remoteAddr net.Addr, from string, to []string) {
+			bytesReceived = bytesRead
+			bytesReceived = 1611 // TODO
+		},
+	}
+
+	go func() {
+		b.Fatal(server.ListenAndServe())
+	}()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+
+		to := []string{"recipient@example.com"}
+		msg, err := ioutil.ReadAll(SampleEmailBody())
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		err = smtp.SendMail(addr, nil, "sender@example.com", to, msg)
+		if err != nil {
+			b.Fatal(err)
+		}
+
+		if bytesReceived != len(msg) {
+			b.Errorf("SMTP send failed, want %d, got %d\n", len(msg), bytesReceived)
+		}
+
 	}
 }
